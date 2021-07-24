5bb2fb3e45eca23667507a5c92da491f88c5cd5e
surbhiigoell <surbhigoel02@gmail.com>
2021-07-24 22:25:38 +0530

    Add constraints

--- android-kotlin-fundamentals-starter-apps-master/DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ android-kotlin-fundamentals-starter-apps-master/DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,9 +17,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -53,8 +52,21 @@ class DevByteApplication : Application() {
     }
 
     private fun setupRecurringWork() {
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
             .build()
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        Timber.d("Periodic Work request for sync is scheduled")
         WorkManager.getInstance().enqueueUniquePeriodicWork(
             RefreshDataWorker.WORK_NAME,
             ExistingPeriodicWorkPolicy.KEEP,
